7af7e0cd6fdf887e74f10f5f2b043d472ac73b6c
GMMDMDIDEMS <jan.kautenburger@gmail.com>
2024-09-06 10:54:10 +0200

    Add types, template_file_path, instance, ungrounded_template_data fixtures and respective scope

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,17 +1,30 @@
+from typing import List
+from pydantic import FilePath
 import pytest
 from rddl_instance_generator.domain import Domain, NonFluent, ObjectType, StateFluent
-from rddl_instance_generator.instance import InstanceGenerator
+from rddl_instance_generator.helper.templater import UngroundedInstanceTemplateData
+from rddl_instance_generator.instance import Instance, InstanceGenerator
 
 
-@pytest.fixture
-def domain(tmp_path):
-    # Create ObjectType instances
-    object_types = [
+@pytest.fixture(scope="session")
+def instance_template_file(tmp_path_factory: pytest.TempPathFactory) -> FilePath:
+    file_path = tmp_path_factory.mktemp("tmp_dir") / "instance_42.rddl"
+    file_path.touch()
+    return file_path
+
+
+@pytest.fixture(scope="session")
+def domain_types() -> List[ObjectType]:
+    return [
         ObjectType(name="object_1", kind="object", alias="obj1"),
         ObjectType(name="object_2", kind="object", alias="obj2"),
     ]
 
-    # Create NonFluent instances
+
+@pytest.fixture(scope="session")
+def domain(domain_types: List[ObjectType]):
+    object_types = domain_types
+
     non_fluents = [
         NonFluent(
             name="non_fluent",
@@ -21,7 +34,6 @@ def domain(tmp_path):
         )
     ]
 
-    # Create StateFluent instances
     state_fluents = [
         StateFluent(
             name="state_fluent",
@@ -31,18 +43,39 @@ def domain(tmp_path):
         )
     ]
 
-    # Create and return the Domain instance
-    domain = Domain(
+    return Domain(
         name="domain",
         domain_alias="domain",
         types=object_types,
         non_fluents=non_fluents,
         state_fluents=state_fluents,
     )
-    # domain.get_template_folder = lambda: tmp_path
-    return domain
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
+def ungrounded_template_data(
+    domain_types: List[ObjectType],
+) -> UngroundedInstanceTemplateData:
+    return UngroundedInstanceTemplateData(
+        domain_alias="test_domain",
+        identifier="5_10",
+        types=domain_types,
+        object_lengths={"object_1": 5, "object_2": 10},
+    )
+
+
+@pytest.fixture(scope="session")
+def instance(
+    ungrounded_template_data: UngroundedInstanceTemplateData,
+    instance_template_file: FilePath,
+) -> Instance:
+    return Instance.from_ungrounded(
+        ungrounded_data=ungrounded_template_data,
+        size=15,
+        template_path=instance_template_file,
+    )
+
+
+@pytest.fixture(scope="session")
 def instance_generator(domain: Domain):
     return InstanceGenerator(domain=domain, num_instances=1, size=2, seed=42)
