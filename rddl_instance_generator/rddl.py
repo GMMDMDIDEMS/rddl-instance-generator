2b205a1c4340f19afab22b6057a07f28a8039afb
GMMDMDIDEMS <jan.kautenburger@gmail.com>
2024-09-08 21:13:45 +0200

    Add basic sampling for fluents of type float

--- rddl_instance_generator/rddl.py
+++ rddl_instance_generator/rddl.py
@@ -11,7 +11,7 @@ from pydantic import FilePath
 
 from rddl_instance_generator.domain import Domain
 from rddl_instance_generator.helper.templater import (
-    GroundedInstanceTemplate,
+    GroundedInstanceTemplateData,
     get_instance_template,
 )
 from rddl_instance_generator.instance import Instance
@@ -44,39 +44,72 @@ class RDDL:
         return v.replace("___", "(").replace("__", ",") + ")"
 
     def get_file_path(self) -> FilePath:
-        return Path(
+        path = Path(
             str(self.domain.get_template_folder()).replace("templates", ""),
             f"size_{self.instance.size}",
         )
+        path.mkdir(parents=True, exist_ok=True)
+        return path
 
     def get_random_sample(
         self,
         pvars: List[str],
         min_range: Union[float, int],
         max_range: Union[float, int],
+        seed: int,
     ) -> List[str]:
-        # TODO pvar must be of type bool, as we define percentage ranges
-
         num_vars = len(pvars)
         num_min = int(min_range * num_vars)
         num_max = int(max_range * num_vars)
 
-        random.seed(self.seed)
+        random.seed(seed)
         # number of grounded vars which will be set to True
         num = random.randint(num_min, num_max)
         num = max(1, num)
 
         # indexes of grounded vars which will be set to True
         indexes = random.sample(range(num_vars), num)
+        # print(indexes)
 
         return [str(pvars[index]) for index in sorted(indexes)]
+
+    def get_random_float_sample(
+        self,
+        pvars: List[str],
+        min_range: Union[float, int],
+        max_range: Union[float, int],
+        seed: int,
+    ) -> List[str]:
+        num_vars = len(pvars)
+
+        random.seed(seed)
+
+        num = random.randint(1, num_vars)
+
+        indexes = random.sample(range(num_vars), num)
+
+        sampled_vars: List[str] = []
+
+        for index in sorted(indexes):
+            sampled_value = random.uniform(min_range, max_range)
+            sampled_vars.append(f"{pvars[index]} = {sampled_value}")
+
+        return sampled_vars
 
     def set_pvar_groundings(self) -> Tuple[List[str], List[str]]:
         nfs: List[str] = []
         sfs: List[str] = []
 
+        # set instance seed and sample fluent seeds
+        random.seed(self.seed)
+        seeds = random.sample(
+            range(10_000), len(self.non_fluents) + len(self.state_fluents)
+        )
+        seed_index = 0
+
         for nf in self.domain.non_fluents:
             nf_name = nf.name
+            type_value = nf.type_value
             min_range = nf.sampling_range["min"]
             max_range = nf.sampling_range["max"]
 
@@ -87,17 +120,36 @@ class RDDL:
                     map(self.convert_grounded_to_instance, nf_variables)
                 )
 
-                nfs.extend(
-                    self.get_random_sample(
-                        pvars=converted_nf_variables,
-                        min_range=min_range,
-                        max_range=max_range,
+                if type_value == "bool":
+                    nfs.extend(
+                        self.get_random_sample(
+                            pvars=converted_nf_variables,
+                            min_range=min_range,
+                            max_range=max_range,
+                            seed=seeds[seed_index],
+                        )
                     )
-                )
+                else:
+                    nfs.extend(
+                        self.get_random_float_sample(
+                            pvars=converted_nf_variables,
+                            min_range=min_range,
+                            max_range=max_range,
+                            seed=seeds[seed_index],
+                        )
+                    )
+            else:
+                if min_range != max_range:
+                    nfs.extend([f"{nf_name} = {random.uniform(min_range, max_range)}"])
+                else:
+                    pass
+
+            seed_index += 1
 
         # set init-state
         for sf in self.domain.state_fluents:
             sf_name = sf.name
+            type_value = sf.type_value
             min_range = sf.sampling_range["min"]
             max_range = sf.sampling_range["max"]
 
@@ -108,13 +160,26 @@ class RDDL:
                     map(self.convert_grounded_to_instance, sf_variables)
                 )
 
-                sfs.extend(
-                    self.get_random_sample(
-                        pvars=converted_sf_variables,
-                        min_range=min_range,
-                        max_range=max_range,
+                if type_value == "bool":
+                    sfs.extend(
+                        self.get_random_sample(
+                            pvars=converted_sf_variables,
+                            min_range=min_range,
+                            max_range=max_range,
+                            seed=seeds[seed_index],
+                        )
                     )
-                )
+                else:
+                    sfs.extend(
+                        self.get_random_float_sample(
+                            pvars=converted_sf_variables,
+                            min_range=min_range,
+                            max_range=max_range,
+                            seed=seeds[seed_index],
+                        )
+                    )
+
+            seed_index += 1
 
         return nfs, sfs
 
@@ -126,15 +191,9 @@ class RDDL:
 
         nfs, sfs = self.set_pvar_groundings()
 
-        context: GroundedInstanceTemplate = {
-            "domain_name": self.domain.name.lower(),
-            "domain_alias": self.domain.domain_alias,
-            "file_id": self.instance.identifier,
-            "types": self.domain.types,
-            "object_length": self.instance.object_lengths,
-            "non_fluents": nfs,
-            "init_state": sfs,
-        }
+        context = GroundedInstanceTemplateData.from_ungrounded(
+            self.instance, non_fluents=nfs, init_state=sfs
+        )
 
         instance_file = template.render(context)
 
